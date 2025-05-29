4db3a636a2724110e7a3d019d70894f8e7d7e273
MURATA Mitsuharu <hikari.photon+dev@gmail.com>
2025-05-30 03:13:41 +0900

    末尾の改行を削除 setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -22,6 +22,7 @@ function Update-FileContent {
     )
     $content = Get-Content -Path $filePath -Raw
     $content = $content -replace $oldValue, $newValue
+    $content = $content.TrimEnd()
     Set-Content -Path $filePath -Value $content
     Write-Host "$filePath 内の参照を更新しました"
 }
